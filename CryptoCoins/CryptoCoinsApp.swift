14d515bb6627ed3f749aaa498d04ade50e36d7db
Alexander <a.pride.xxi@tutanota.com>
2023-07-07 10:37:13 +0300

    Initital Commit

---
+++ CryptoCoins/CryptoCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinsApp.swift
+//  CryptoCoins
+//
+//  Created by Александр Прайд on 07.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
