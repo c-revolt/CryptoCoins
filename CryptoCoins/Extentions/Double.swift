7e8552035ddda87d672bc17decb23649055bf8e1
Alexander <a.pride.xxi@tutanota.com>
2023-07-10 19:01:30 +0300

    feat: Created Design for a row

---
+++ CryptoCoins/Extentions/Double.swift
@@ -0,0 +1,35 @@
+//
+//  Double.swift
+//  CryptoCoins
+//
+//  Created by Александр Прайд on 10.07.2023.
+//
+
+import Foundation
+
+extension Double {
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+//        formatter.currencyCode = "usd"
+//        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    func asPercentString() -> String {
+        return asNumberString() + "%"
+    }
+}
