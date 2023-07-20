629023fdff300e30c2cb69c204dad73eff24dcf2
Alexander <a.pride.xxi@tutanota.com>
2023-07-20 10:23:38 +0300

    feat: Filtering Coins

--- CryptoCoins/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoCoins/Core/Home/ViewModels/HomeViewModel.swift
@@ -13,7 +13,7 @@ final class HomeViewModel: ObservableObject {
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
     
-    private let coinDataServive = CoinDataService()
+    private let coinDataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
     
     init() {
@@ -21,10 +21,30 @@ final class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        coinDataServive.$allCoins
+    
+        // updates allCoins
+        $searchText
+            .combineLatest(coinDataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filteringCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filteringCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
