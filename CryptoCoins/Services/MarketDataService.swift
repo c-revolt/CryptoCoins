5988175692168760f064817b908078e73ece6007
Alexander <a.pride.xxi@tutanota.com>
2023-08-09 18:50:22 +0300

    feat: Reload data from API

--- CryptoCoins/Services/MarketDataService.swift
+++ CryptoCoins/Services/MarketDataService.swift
@@ -17,7 +17,7 @@ final class MarketDataService {
         getData()
     }
     
-    private func getData() {
+    func getData() {
         
         guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
         
